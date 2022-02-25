e6c99454cf8b878c10f3c501764dc80687aecfc8
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-02-25 11:42:35 -0500

    groovy:4.0.0

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -7,8 +7,7 @@ dl () {
     local file="apache-groovy-binary-${ver}.zip"
     local url="${MIRROR}/${ver}/distribution/${file}.sha256"
     printf "  # %s\n" $url
-    printf "  '%s': sha256:%s\n" $ver $(curl -sSL $url)
+    printf "  '%s': sha256:%s\n" $ver $(curl -sSLf $url)
 }
 
-dl ${1:-3.0.9}
-
+dl ${1:-4.0.0}
